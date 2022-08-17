7d5eb427f18f23e23b8259b4b2d77fba8ac1fcb1
devopsor <saekiemail@gmail.com>
2022-08-17 23:27:37 +0900

    update pandas tutorials

---
+++ 22_pandas/02.jupyter.py
@@ -0,0 +1,45 @@
+#################################Jupyter######################################
+
+#What is Jupyter?
+# Jupyter is a web application(※) that acts like a container for data science projects. 
+# It allows you to put data, code, visualizations, documentation, and more into a single notebook.
+
+# I’ll be honest, if you’re an experienced software developer who is accustomed to an IDE like 
+# Eclipse or a text editor like Visual Studio Code, Jupyter is going to seem weird.
+
+# Jupyter is, essentially, a modern reincarnation of Donald Knuth’s Literate Programming. 
+# Literate programming aims to break down the barriers between code and natural language. 
+# In a typical literate programming file, programming code is interspersed with prose in a natural English-
+# like language that describes what the code does.
+
+# This approach might sound repugnant to modern developers. 
+# After all, shouldn’t your code be so clear and self-explanatory that it doesn’t need comments?
+
+# That may be true for ordinary, mechanical code where it’s clear what is going on. 
+# But the situation is different when you’re writing code for data science and machine learning projects. 
+# In these scenarios, you’re often writing code that’s going to be consumed by a wider audience including 
+# data scientists, business analysts, and even managers.
+
+# In those cases, your code alone isn’t enough. Even if the reader understands the code, you must add 
+# prose to give context to your code — to help readers understand why you wrote the code you did, 
+# and understand how your code is transforming the data it imports.
+
+# Jupyter Notebooks take literate programming a step further. Not only is it easy to write a document that 
+# alternates between prose and code, the code is also live and executable. You can run the code and 
+# observe its output from inside the document. Even better, colleagues who have a copy of your notebook 
+# can edit your code, re-run it, and observe the new output — all without leaving the notebook. 
+# A notebook that contains code, prose, and output
+
+
+#Installation
+# pip install jupyterlab
+
+print('------------------------Welcome to Jupyter!---------------------')
+print('1. Installation methods:')
+print('pip install jupyterlab')
+print('---------------------------------------------------------------')
+print('2. Execute methods:')
+print('jupyter notebook')
+# https://insights.stackoverflow.com/survey
+
+# https://qiita.com/ysdyt/items/9ccca82fc5b504e7913a
---
+++ 22_pandas/03.readcsv.py
@@ -0,0 +1,20 @@
+#################################Jupyter######################################
+
+print('2. Execute methods:')
+print('jupyter notebook')
+
+# import pandas as pd
+# df = pd.read_csv('survey_results_public.csv')
+# schema_df = pd.read_csv('survey_results_schema.csv')
+
+# pd.set_option('display.max_columns', 5)
+# pd.set_option('display.max_rows', 10)
+
+# df
+# df.shape
+# df.info()
+# df.head ()
+# schema_df
+
+# API
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.info.html
